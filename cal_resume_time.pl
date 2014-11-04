bc9df521b03fcae16fb74212c2bee5115fa55b3e
piter_hsu <Piter_Hsu@asus.com>
2014-11-04 11:43:32 +0800

    cal_resume_time: recognize "who cause the delay"
    1. main code
        $delay_time_kernel = $PM_request_wake_time - $btn_press_time;
        $delay_time_AP = $display_on_time - $PM_request_wake_time;
    
        if($delay_time_AP > $delay_time_kernel){
    	$who_cause_the_delay = "AP cause";
        } else{
    	$who_cause_the_delay = "kernel cause";
        }

--- cal_resume_time.pl
+++ cal_resume_time.pl
@@ -10,52 +10,97 @@ set_output($dirname);
 arg_parse();
 
 print "Reading $inputfile ...\n\n";
-#$display_on = "innolux_n070iceg02_vid_set_brightness: brightness level";  # 375 case
-$display_on = "mdfld_dsi_auo_set_brightness: brightness level";   # 581 case
+$display_on = "innolux_n070iceg02_vid_set_brightness: brightness level=";  # 375 case
+#$display_on = "mdfld_dsi_auo_set_brightness: brightness level=";   # 581 case
+#$display_on = "lvds_chimei_set_backlight ";   # 103k case
+
 $resume_btn_press = "wakeup from";
+#$resume_btn_press = "msm_pm_enter: return";	# 103k case
+
 $device_suspend = "PM: suspend entry";
+$PM_request_wake = "request_suspend_state: wakeup";
 
 $resume_time = 0;
 $flag = 0;
 $btn_press_time = 0;
 $total_times = 0;
+$PM_request_wake_time = 0;
+
+$who_cause_the_delay = "no one";
+$delay_time_kernel = 0;
+$delay_time_AP = 0;
 
 $t1=0,$t2=0,$t3=0,$t4=0;
 
 open (FHD, "$inputfile") or die "Cannot open files. $!\n";
 open (ab_file,">$outputfile3");
-print ab_file "Button_Press_Time\tDisplay_on_Time\t\tResume_Time\n";
+print ab_file "Button_Press_Time\tPM_request_wake\t\tDisplay_on_Time\t\tResume_Time\t\tWho cause\n";
+print ab_file "============================================================================================================\n";
 open (wr_file,">$outputfile");
 print wr_file "Inputfile:\t$inputfile\n\n";
-print wr_file "Button_Press_Time\tDisplay_on_Time\t\tResume_Time\n";
+print wr_file "Button_Press_Time\tPM_request_wake\t\tDisplay_on_Time\t\tResume_Time\t\tWho cause\n";
+print wr_file "============================================================================================================\n";
 
 while($line = <FHD>){
-	if ($line =~ m/([0-9]+\.[0-9]+)] +$resume_btn_press/){
-		$btn_press_time = $1;
-		$flag = 0;
-	}
-	elsif ($line =~ m/([0-9]+\.[0-9]+)] +$device_suspend/){
-		$flag = 1;
-	}
-	elsif ($line =~ m/([0-9]+\.[0-9]+)] +$display_on=([0-9]+)/){
-		$display_on_time = $1;
-		$level = $2;
-		if($flag == 0 && $level != 0 && $btn_press_time != 0){
+	if($flag == 0){
+		if ($line =~ m/([0-9]+\.[0-9]+)] +$resume_btn_press/){
+			$btn_press_time = $1;
 			$flag = 1;
-			$resume_time = $display_on_time - $btn_press_time;
-			print wr_file "$btn_press_time\t\t$display_on_time\t\t$resume_time\n";
-			$total_times = $total_times + 1;
-			# make statistic for different resume time
-			if($resume_time < 0.5){
-				$t1 = $t1 + 1;
-			}elsif($resume_time < 1){
-				$t2 = $t2 + 1;
-			}elsif($resume_time < 1.5){
-				$t3 = $t3 + 1;
-			}else{
-				print ab_file "$btn_press_time\t\t$display_on_time\t\t$resume_time\n";
-				$t4 = $t4 + 1;
+		}
+	} elsif ($flag == 1){
+		if ($line =~ m/([0-9]+\.[0-9]+)] +$resume_btn_press/){  # meet resume_btn_press again.
+                        $btn_press_time = $1;				 # reset btn_press_time.
+                        $flag = 1;
+                } elsif ($line =~ m/([0-9]+\.[0-9]+)] +$device_suspend/){ # meet device_suspend.
+			$flag = 0;					  # back to flag 0.
+		} elsif ($line =~ m/([0-9]+\.[0-9]+)] +$PM_request_wake/){
+			$PM_request_wake_time = $1;
+			$flag = 2;
+		}
+	} elsif ($flag == 2){
+		if ($line =~ m/([0-9]+\.[0-9]+)] +$display_on([0-9]+)/){
+			$level = $2;
+			if ($level != 0 && $btn_press_time != 0){ # brightness level !=0
+				$flag = 3;
+				$display_on_time = $1;
+
+				$resume_time = $display_on_time - $btn_press_time;
+
+				$delay_time_kernel = $PM_request_wake_time - $btn_press_time;
+				$delay_time_AP = $display_on_time - $PM_request_wake_time;
+
+				if($delay_time_AP > $delay_time_kernel){
+					$who_cause_the_delay = "AP cause";
+				} else{
+					$who_cause_the_delay = "kernel cause";
+				}
+
+	                        #print wr_file "$btn_press_time\t\t$PM_request_wake_time\t\t$display_on_time\t\t$resume_time\t\t$who_cause_the_delay\n";
+	                        printf wr_file "%f\t\t%f\t\t%f\t\t%.6f\t\t%s\n", $btn_press_time, $PM_request_wake_time, $display_on_time, $resume_time, $who_cause_the_delay;
+				$total_times = $total_times + 1;
+				# make statistic for different resume time
+				if($resume_time < 0.5){
+					$t1 = $t1 + 1;
+				}elsif($resume_time < 1){
+					$t2 = $t2 + 1;
+				}elsif($resume_time < 1.5){
+					$t3 = $t3 + 1;
+				}else{
+					#print ab_file "$btn_press_time\t\t$PM_request_wake_time\t\t$display_on_time\t\t$resume_time\t\t$who_cause_the_delay\n";
+					printf ab_file "%f\t\t%f\t\t%f\t\t%.6f\t\t%s\n", $btn_press_time, $PM_request_wake_time, $display_on_time, $resume_time, $who_cause_the_delay;
+					$t4 = $t4 + 1;
+				}
+
+
+				$flag = 0;
 			}
+		} elsif ($line =~ m/([0-9]+\.[0-9]+)] +$resume_btn_press/){
+			$btn_press_time = $1;
+			$flag = 1;
+		} elsif ($line =~ m/([0-9]+\.[0-9]+)] +$device_suspend/){
+			$$flag = 0;
+		} elsif ($line =~ m/([0-9]+\.[0-9]+)] +$PM_request_wake/){
+			$flag = 0;
 		}
 	}
 }
@@ -166,6 +211,7 @@ sub arg_parse{
 }
 
 sub help{
+		print "\n";
 		print "Usage: ./cal_resume_time.pl [\$INPUTFILE] [-o \$OUTDIR]\n";
 		print "       ./cal_resume_time.pl [-o \$OUTDIR] [\$INPUTFILE]\n\n\n";
 		print "Example: ./cal_resume_time.pl kernel_1.log            <- inputfile \"kernel_1.log\" output folder \"result\".\n";
